bc791c68bd3ac5c575abf04b41d6e6d4364aae6d
albyh <albyheredia@gmail.com>
2016-09-07 13:53:20 -0700

    mongolabs persistence added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+var db; 
+
+MongoClient.connect('mongodb://admin:tutadmin@ds019856.mlab.com:19856/expersstut', (err, database) => {
+	if (err) return console.log(err);
+	db = database;
+	app.listen(3000, () => {
+		console.log("listening on 3000")
+	});
+});
 
 app.use(bodyParser.urlencoded({extended: true}))
-
-app.listen(3000, function(){
-	console.log("listening on 3000")
-})
 
 app.get('/', (req, res) => {
 	//res.send("response from root");
@@ -13,7 +20,18 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 });
 
+app.get("/quotes", (req, res) => {
+	console.log("wrong access point... let me help you... redirecting to /")
+	res.redirect('/')
+})
+
 app.post("/quotes", (req, res) => {
-	console.log("received a post request at /")
+	console.log(req.body)
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
 })
 
